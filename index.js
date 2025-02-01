1e0b74bf2ca5481b0c1d6a32c2354067ad0f1bc7
Abhishek  Gaikwad <abhishekgaikwad065@gmail.com>
2025-02-01 14:47:46 +0530

    Update index.js
    kuch gadab thi code me fix kar di hai 🥲

--- index.js
+++ index.js
@@ -15,50 +15,53 @@ It performs wrong operation 10% of the times
 */
 
 let firstNum = Number(prompt("enter first number"));
-let userOperator = (prompt("enter Operator (-, +, *, /, **) "));
+let userOperator = prompt("enter Operator (-, +, *, /, **) ");
 let secNum = Number(prompt("enter Sec number"));
 
 //faulty calc function
 
 let faultyCalc = () => {
-  if (userOperator == "+") {
-    console.log(firstNum - secNum); //Miltiply karga
-  } else if (userOperator == "*") {
-    console.log(firstNum + secNum); //minus karga
-  } else if (userOperator == "-") {
-    console.log(firstNum / secNum); //divide karga
-  } else if (userOperator == "/") {
-    console.log(firstNum ** secNum); //kuch to karga
+  if (userOperator === "+") {
+    alert(firstNum - secNum);
+  } else if (userOperator === "*") {
+    alert(firstNum + secNum);
+  } else if (userOperator === "-") {
+    alert(firstNum / secNum);
+  } else if (userOperator === "/") {
+    alert(firstNum ** secNum);
+  } else if (userOperator === "**") {
+    alert(firstNum + secNum);
   } else {
-    console.log("kuch to gad bad hai");
+    alert("faultyCalc me kuch to gad bad hai");
   }
 };
 
-let corectCalc = () => {
-    if (userOperator == "*") {
-      console.log(firstNum - secNum); //Miltiply karga
-    } else if (userOperator == "-") {
-      console.log(firstNum + secNum); //minus karga
-    } else if (userOperator == "/") {
-      console.log(firstNum / secNum); //divide karga
-    } else if (userOperator == "**") {
-      console.log(firstNum ** secNum); //kuch to karga
-    } else {
-      console.log("kuch to gad bad hai");
-    }
-  };
+let correctCalc = () => {
+  if (userOperator === "*") {
+    alert(firstNum * secNum);
+  } else if (userOperator === "+") {
+    alert(firstNum + secNum);
+  } else if (userOperator === "-") {
+    alert(firstNum - secNum);
+  } else if (userOperator === "/") {
+    alert(firstNum / secNum);
+  } else if (userOperator === "**") {
+    alert(firstNum ** secNum);
+  } else {
+    alert("correctCalc me kuch to gad bad hai");
+  }
+};
 
+//10% faulty aur 90% correct calc ke liye logic
 
-//10% faulty aur 90% sahi calc ke liye logic
+let random = Math.floor(Math.random() * 100);
 
-let random = Math.floor(Math.random() * 100)
-
-if(11 > random){
-    console.log(random, "randome num")
-    console.log("10% work kar rha hai")
-    faultyCalc()
+if (10 >= random) {
+  console.log(random, "random num");
+  console.log("10% work kar rha hai");
+  faultyCalc();
 } else {
-    console.log("work kar rha hai")
-    console.log(random, "randome num")
-    corectCalc()
+  console.log("90% work kar rha hai");
+  console.log(random, "random num");
+  correctCalc();
 }
